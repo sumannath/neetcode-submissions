c21db9e410c7f5409c61da74fba9e0362e9d2bb8
Suman <suman.pro@gmail.com>
2026-08-12 18:14:39 +0530

    Update: is-anagram - submission-2

--- Data Structures & Algorithms/is-anagram/submission-2.py
+++ Data Structures & Algorithms/is-anagram/submission-2.py
@@ -1,17 +1,18 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        hashmapS = {}
-        hashmapT = {}
-        for index, ch in enumerate(s):
-            if ch in hashmapS:
-                hashmapS[ch] += 1 
+        return self.char_freq(s) == self.char_freq(t)
+    
+    def char_freq(self, s: str) -> dict:
+        freq = {}
+        for x in s:
+            if x in freq:
+                freq[x] = freq[x] + 1    
             else:
-                hashmapS[ch] = 1
+                freq[x] = 1
 
-        for index, ch in enumerate(t):
-            if ch in hashmapT:
-                hashmapT[ch] += 1 
-            else:
-                hashmapT[ch] = 1
+        return freq
 
-        return hashmapS == hashmapT
+
+
+
+        
